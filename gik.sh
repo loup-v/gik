215271cb06cba7bd06ac8f1b37778551784905ee
Lukasz <lukasz@intheloup.io>
2018-01-23 18:15:15 -0500

    Fix curl issue.

--- gik.sh
+++ gik.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "fetching script"
-curl https://raw.githubusercontent.com/loup-studio/Gik/master/gik.sh -o .gik.sh >/dev/null || exit 1
+curl https://raw.githubusercontent.com/loup-studio/Gik/master/gik-base.sh -s -o .gik.sh >/dev/null || exit 1
 chmod +x .gik.sh
 
 function clean() {
